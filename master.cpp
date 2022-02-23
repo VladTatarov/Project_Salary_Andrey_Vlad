fdd906a9b37212d64a312199ef18396be9a6f7b8
karmikday@gmail.com <karmikday@gmail.com>
2022-02-23 20:38:13 +0200

    Done-3

--- master.cpp
+++ master.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
-template <typename T>
-T SortAlgo(T arr[10],T lenD)
+template<typename T>
+T SortAlgo(T arr[],T lenD)
 {
     lenD = 10;
     int j = 0;
     int tmp = 0;
-    for (int i = 0; i < lenD; i++) 
-    {
+    for (int i = 0; i < lenD; i++) {
         j = i;
-        for (int k = i; k < lenD; k++) 
-        {
-            if (arr[j] > arr[k])
-            {
+        for (int k = i; k < lenD; k++) {
+            if (arr[j] > arr[k]) {
                 j = k;
             }
         }
