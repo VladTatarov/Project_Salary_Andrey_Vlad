3d54ec11e4253189bcf81a40115d29c893976b42
karmikday@gmail.com <karmikday@gmail.com>
2022-02-23 20:03:24 +0200

    update

--- master.cpp
+++ master.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
-template <typename T>
-T SortAlgo(T arr[10],T lenD)
-{
-    lenD = 10;
-    int j = 0;
-    int tmp = 0;
-    for (int i = 0; i < lenD; i++) 
-    {
-        j = i;
-        for (int k = i; k < lenD; k++) 
-        {
-            if (arr[j] > arr[k])
-            {
-                j = k;
-            }
-        }
-        tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-}
 int main()
 {
-	
+	int ex;
+	switch (ex)
+	{
+	case 1:
+	{
+
+	}
+	case 2:
+	{
+
+	}
+	case 3:
+	{
+
+	}
+	case 4:
+	{
+
+	}
+	default:
+		break;
+	}
 }
