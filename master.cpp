00db8321d46a2323d232230fb8da22b65328847e
student <student@COMP408>
2022-02-23 20:04:44 +0200

    conflict

---
+++ master.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<Windows.h>
+
+
+
+using namespace std;
+
+template <typename T>
+T PrintArr(T arr)
+{	
+	size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+	{
+
+	}
+	cout << arr << endl;
+}
+template <typename T>
+T RandArr(T arr)
+{
+	
+	size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr = rand() % 10 
+	}
+	
+	
+}
+
+int main()
+{
+	int ex;
+	switch (ex)
+	{
+	case 1:
+	{
+
+
+	
+
+	}
+	case 2:
+	{
+
+
+
+
+	}
+	default:
+		break;
+	}
+}
