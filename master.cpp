732ca3c665540c453e085e9a3c8788896fe92f9e
Tata_kn18 <karmikday@gmail.com>
2022-03-02 18:16:06 +0200

    update

--- master.cpp
+++ master.cpp
@@ -5,25 +5,39 @@ template<typename T>
 void MaxElement(T arr[])
 { 
     T maxvalue = arr[0];
-    for (int i = 0; i < 10; i++) 
+    for (int i = 0; i < 12; i++) 
     {
         if (arr[i] > maxvalue) 
         {
             maxvalue = arr[i];
         }
     }
-    cout << "Maxvalue: " << maxvalue;
+    cout << "—амый удачный мес€ц: " << maxvalue;
 }
+template<typename T>
+void Max(T arr[])
+{
+    T maxvalue = arr[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > maxvalue)
+        {
+            maxvalue = arr[i];
+        }
+    }
+    cout << "\n—амый удачный за 5 мес.=>:" << maxvalue;
+}
+
 
 template <typename T>
 void PrintArr(T arr[])
 {	
     srand(time(0));
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 12; i++)
     {
-        arr[i] = rand() % 1000;
-        cout << "[" << i+1 << "]" << endl;
-        cout << arr << endl;
+        arr[i] = rand() % 10000;
+        cout << arr[i] << " " << endl;
+      
     }
 }
 template<typename T>
@@ -31,25 +45,31 @@ void SortAlgo(T arr[])
 {
     int j = 0;
     int tmp = 0;
-    for (int i = 0; i < 10; i++) 
+    for (int i = 0; i < 12; i++) 
     {
         j = i;
-        for (int k = i; k < 10; k++) 
-{
-            if (arr[j] > arr[k]) {
+        for (int k = i; k < 12; k++) 
+        {
+            if (arr[j] > arr[k]) 
+            {
                 j = k;
             }
         }
         tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+        cout << "[" << i + 1 << "]";
+        cout << arr[i] << " " << endl;
     }
+    
 }
 int main()
 {
-    int arr[10];
+    setlocale(LC_ALL, "Russian");
+    int arr[12];
     PrintArr(arr);
     SortAlgo(arr);
     MaxElement(arr);
+    Max(arr);
 }
    
